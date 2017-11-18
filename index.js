bc8b5fb9614f6587c65f44315be1bd011480ff14
Lachlan <lachlan_young@hotmail.com>
2017-11-18 15:20:39 +1000

    Populated Bomba

--- index.js
+++ index.js
@@ -5,9 +5,39 @@ let path = require('path');
 let app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-app.listen('8080');
+app.listen('9000');
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/profile/index.html'));
-})
+let horseInfo = {
+  horsename: "Auztin",
+  horsetype: "Stallion",
+  appointment: "19/11/2017",
+  dob: "23/05/1996",
+  breed: "Horse",
+  sire: "Fuzzy",
+  dam: "Wuzzy",
+  gsire: "MrFuz",
+  damsire: "MissWuz",
+  fei: "thing",
+  feed: "corn",
+  supplements: "steroids",
+  rugging: "idk",
+  medications: "panadol",
+  notes: "I dont understand horses",
+  schedmon: "Eat",
+  shedtues: "Drink",
+  schedwed: "Sleep",
+  schedthurs: "Pray",
+  schedfri: "Repeat",
+  schedsat: "Misc",
+  schedsun: "Date Night"
+};
+
+app.get('/api/horse', loadHorse);
+
+console.log("Connected")
+
+function loadHorse(req, res){
+  console.log(req.query);
+  res.json(horseInfo);
+}
